7041ac07a123a4ab19950e6fa77fd7c30ec9e0f3
Aark Koduru <aark@Aarks-MacBook-Pro.local>
2020-03-17 15:40:38 -0700

    stable version of SCC

--- python/SSC.py
+++ python/SSC.py
@@ -9,17 +9,31 @@ import scipy as sp
 import math
 from numpy import linalg as lin
 
-def SSC(Y,rho,lam,maxIter):
+def Tau(v, eta):
     r"""
+    Shrinkage-thresholding operator
+    Inputs:
+    -------
+        v: Matrix size N x N
+        eta: threshold
 
+    Outputs:
+    -------
+        result: Matrix size N x N
+    """
+    return np.maximum(0,np.abs(v) - (eta * np.ones_like(v))) * np.sign(v)
+
+def SSC(Y,rho,alpha_z,alpha_e,maxIter,eps=1e-4):
+    r"""
 
     Inputs:
     -------
         Y: data matrix of size D x N
-        rho:
-        lam:
-        maxIter:
-
+        rho: Objective function parameter, value > 0
+        alpha_z: Balencing term for Z, value > 1
+        alpha_e: Balencing term for E, value > 1
+        maxIter: Total number of iterations
+        eps: Error tolerance, 1e-4 recommended
     Outputs:
     -------
         C: Optimal sparse coefficient matrix of size N x N
@@ -31,3 +45,42 @@ def SSC(Y,rho,lam,maxIter):
     Delta = np.zeros((N,N))
     delta = np.zeros((N,1))
     E = np.zeros((D,N))
+    yty = np.matmul(Y.T,Y)
+    yty = np.abs(yty - np.diag(np.diag(yty)))
+    lam_z = alpha_z/np.min(np.max(yty))
+    lam_e = np.min(lin.norm(Y,axis=1)) #as per John's suggestion
+
+    one_one_T = np.matmul(np.ones((N,1)),np.ones((1,N)))
+    one_matrix = np.ones((N,N))
+    left = lam_z * (yty + rho * np.eye(N) + rho * one_one_T)
+
+    for kk in range(maxIter):
+        E_prev = E
+        A_prev = A
+        #Updating A using CG solver
+        right =  lam_z * np.matmul(Y.T,(Y - E)) + rho * (one_one_T + C) - np.matmul(np.ones((N,1)),delta.T) - Delta
+        for  i in range(len(right)):
+            A[:,i] = sp.sparse.linalg.cgs(left,right[:,i])[0]
+
+        #Updating C
+        v = A + Delta * (1/rho)
+        J = Tau(v, 1/rho)
+        C = J - np.diag(np.diag(J))
+
+        #Updating E
+        v = Y - np.matmul(Y,A)
+        E = Tau(v,lam_e/lam_z)
+
+        #Updating delta
+        delta = delta + rho * (np.matmul(A.T,one_matrix) - one_matrix)
+
+        #Updating Delta
+        Delta = Delta + rho * (A - C)
+
+        #error check
+        if lin.norm((np.matmul(A.T,one_matrix) - one_matrix), np.inf) <= eps\
+            and lin.norm(A-C,np.inf) <= eps and lin.norm(A-A_prev,np.inf)<= eps\
+            and lin.norm(E-E_prev,np.inf) <= eps:
+            break
+
+    return C
--- python/synthetic_test.py
+++ python/synthetic_test.py
@@ -1,10 +1,13 @@
 from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from SSC import SSC
 #Generating synthetic data
 centers = [(-10, -6), (7, 7)]
 cluster_std = [0.8, 1]
 
 X, y = make_blobs(n_samples=100, cluster_std=cluster_std,
                 centers=centers, n_features=2, random_state=1)
+
+C = SSC(X.T,0.1,0.1,0.1,1)
+print(C.shape)
