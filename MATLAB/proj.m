d9d3c7cd96b779ffb4b8545ac18d3b98d0d9f4f5
Jesse Zhu <33672921+jzcs@users.noreply.github.com>
2020-03-17 20:45:45 -0700

    sparse ADMM stable

---
+++ MATLAB/proj.m
@@ -0,0 +1,27 @@
+﻿% Math ML Winter 2020
+% Final Project on Sparse Subspace Clustering (2013 Elhamifar,Vidal)
+
+clear
+close all
+
+%Setting 1-d and 2-d subspaces in 3D space (for visualization)
+D = 3; %Dimension of ambient space (increase for sparsity)
+d1 = 1; d2 = 1; %d1 and d2: dimension of subspace 1 and 2
+N1 = 20; N2 = 20; %N1 and N2: number of points in subspace 1 and 2
+X1 = randn(D,d1) * randn(d1,N1); %Generating N1 points in a d1 dim. subspace
+X2 = randn(D,d2) * randn(d2,N2); %Generating N2 points in a d2 dim. subspace
+
+d3 = 2; N3 = 50;
+X3 = randn(D,d3) * randn(d3,N3); 
+X = [X1 X2 X3];
+s = [1*ones(1,N1) 2*ones(1,N2) 3*ones(1,N3)];
+
+figure
+scatter3(X1(1,:),X1(2,:),X1(3,:),100,'r')
+hold all
+scatter3(X2(1,:),X2(2,:),X2(3,:),100,'b')
+scatter3(X3(1,:),X3(2,:),X3(3,:),100,'g')
+
+disp("norm of X - XC");
+C = sparseADMM(X);
+norm(X-X*C)
---
+++ MATLAB/sparseADMM.m
@@ -0,0 +1,84 @@
+﻿% Math ML 2020
+% E. Elhamifar and R. Vidal, “Sparse subspace clustering..."
+% Algorithm 2 for sparse coefficient matrix
+
+function C = sparseADMM(Y,alpha= 800,maxIter = 200, eps=2e-4)
+%------------------------------------------------------------------
+%Referencing http://khoury.neu.edu/home/eelhami/codes.htm
+% This function takes a DxN matrix of N data points in a D-dimensional 
+% space and returns a NxN coefficient matrix of the sparse representation 
+% of each data point in terms of the rest of the points
+%Inputs
+% Y: DxN data matrix
+%constants
+% alpha(1,2) -> to calculate lambda(1,2)
+% rho -> >0
+% thr1: stopping threshold for the coefficient error ||Z-C||
+% thr2: stopping threshold for the linear system error ||Y-YZ||
+% maxIter: maximum number of iterations of ADMM
+% C: NxN sparse coefficient matrix
+
+%Set constants
+if (length(alpha) == 1)
+    alp_e = alpha(1);
+    alp_z = alpha(1);
+elseif (length(alpha) == 2)
+    alp_e = alpha(1);
+    alp_z = alpha(2);
+end
+
+rho = alp_e;
+
+YtY = Y'*Y;
+mu_e = min(max(Y,[],1));
+mu_z = min(max(abs(YtY - diag(diag(YtY))),[],2)); %max each row
+
+lam_e = alp_e / mu_e;
+lam_z = alp_z / mu_z;
+
+%initialize C, A, E, d, Delta
+[D, N] = size(Y);
+C = zeros(N,N);
+A = zeros(N,N);
+E = zeros(D,N);
+d = zeros(N,1);
+Delta = zeros(N,N);
+
+c1 = lam_z*YtY+rho*eye(N,N)+rho*ones(N,N);
+for i = 1:1:maxIter
+    E_prev = E;
+    A_prev = A;
+
+   %update A
+   %A = inv(c1)*(lam_z * (YtY - Y'*E) + rho*(ones(N,N) + C) - ones(N,1)*d' - Delta); %doesn't work (also don't use / to solve)
+   
+   A = inv(c1)* (lam_z*(YtY)+rho*(C-Delta/rho)+rho*ones(N,1)*(ones(1,N)-d'/rho));
+   
+   %update C
+   C = shrinkThresh(A+Delta/rho, 1/rho);
+   C = C - diag(diag(C));
+   
+   %update E
+   E = shrinkThresh( (Y - Y*A), lam_e/lam_z);
+   
+   %update little delta, d
+   at_ones = A'*ones(N,1) - 1;
+   d = d + rho*(at_ones);
+   
+   %update Delta
+   Delta = Delta + rho*(A-C);
+   
+   %Set break threshold
+   stop = [max(at_ones(:)), max((A-C)(:)), max((A-A_prev)(:)), max((E-E_prev)(:))];
+   if max(stop) <= eps
+       break
+   end
+end
+end
+
+function s = shrinkThresh(v,c)
+    %Inputs
+    %v = input matrix
+    %c = thresholding value
+    s = max(0,(abs(v) - c*ones(size(v)))) .* sign(v);
+end
