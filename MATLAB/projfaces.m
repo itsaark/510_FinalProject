97ab3ccaecfb756f10c6c0c54b44a3c13c5c8707
gigolden <33341226+gigolden@users.noreply.github.com>
2020-03-20 21:37:09 -0700

    benchmark runs on 2,3,5,8,10 clusters

--- MATLAB/projfaces.m
+++ MATLAB/projfaces.m
@@ -9,70 +9,79 @@ clear
 close all
 load YaleBCrop025.mat
 
-nClust = 2;
-s = [];
+nClustSet = [2 3 5 8 10];
+%nClust = 3;
 
-for ii = 1:nClust
-    s = [s ii*ones(1,64)];
+for i = 1:length(nClustSet)
+    nClust = nClustSet(i);
+    s = [];
+
+    for ii = 1:nClust
+        s = [s ii*ones(1,64)];
+    end
+
+    tic
+        idx = Ind{nClust};
+        for j = 1:size(idx,1)
+            X = [];
+            for p = 1:nClust
+                X = [X Y(:,:,idx(j,p))];
+            end        
+            [D,N] = size(X);
+        end
+    toc
+
+    disp("norm of X - XC");
+    C = sparseADMM(X, alpha=1, rho=800);
+    norm(X-X*C)
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %CREATE SIMILARITY GRAPH
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %Normalize by column max, and create similarity graph
+    %C = C ./max(C);
+    C = normc(C);
+    W = abs(C) + abs(C)';
+    
+    %Threshold W in rows and columns to top q values (or median+c)
+    
+    %MEDIAN
+    t = 0.05; %threshold value over median
+    W2 = W.*(W> (median(W(:))+t) ) + 1e-6; %add constant so eig doesn't get Inf/NaN warning message
+    
+    %KEEP q
+    %q = fix(size(W,1)/max(s)); %Setting q to a percentage of N
+    %W2 = threshk(W, q*6 );
+    figure
+    imagesc(W);
+    figure
+    imagesc(W2);
+    title('Thresholded Affinity matrix');
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %SPECTRAL CLUSTERING
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    
+    %USAGE>> [estLabels,Y] = mySpectralClustering(W,K,normalized)
+    [estLabels, _] = mySpectralClustering(W,nClust,1);
+    [estLabels2, _] = mySpectralClustering(W2,nClust,1);
+    
+    s = s';
+    disp(['nclusters=' num2str(nClust)])
+    [err estLabels] = missRate(s, estLabels);
+    disp(['err =' num2str(err)])
+    [err2 estLabels2] = missRate(s, estLabels2);
+    disp(['err2(thresholding) =' num2str(err2)])
+    
+    
+    figure
+    plot(s,'g'); hold on
+    plot(estLabels2,'r');
+    legend('true', 'predicted');
 end
 
-tic
-    idx = Ind{nClust};
-    for j = 1:size(idx,1)
-        X = [];
-        for p = 1:nClust
-            X = [X Y(:,:,idx(j,p))];
-        end        
-        [D,N] = size(X);
-    end
-toc
-
-disp("norm of X - XC");
-C = sparseADMM(X, alpha=1, rho=800);
-norm(X-X*C)
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%CREATE SIMILARITY GRAPH
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%Normalize by column max, and create similarity graph
-%C = C ./max(C);
-C = normc(C);
-W = abs(C) + abs(C)';
-
-%Threshold W in rows and columns to top q values (or median+c)
-
-%MEDIAN
-t = 0.05; %threshold value over median
-W2 = W.*(W> (median(W(:))+t) ) + 1e-6; %add constant so eig doesn't get Inf/NaN warning message
-
-%KEEP q
-%q = fix(size(W,1)/max(s)); %Setting q to a percentage of N
-%W2 = threshk(W, q*6 );
-figure
-imagesc(W);
-figure
-imagesc(W2);
-title('Thresholded Affinity matrix');
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%SPECTRAL CLUSTERING
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%USAGE>> [estLabels,Y] = mySpectralClustering(W,K,normalized)
-[estLabels, _] = mySpectralClustering(W,2,1);
-[estLabels2, _] = mySpectralClustering(W2,2,1);
-
-s = s';
-[err estLabels] = missRate(s, estLabels);
-disp(['err =' num2str(err)])
-[err2 estLabels2] = missRate(s, estLabels2);
-disp(['err2(thresholding) =' num2str(err2)])
-
-
-figure
-plot(s,'g'); hold on
-plot(estLabels2,'r');
-legend('true', 'predicted');
